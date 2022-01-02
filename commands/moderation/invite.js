59c549db0146634524af8c853a2c62a4280b6f60
Lexxy <85262576+Snipedprox@users.noreply.github.com>
2022-01-02 18:53:52 -0300

    Create invite.js

---
+++ commands/moderation/invite.js
@@ -0,0 +1,7 @@
+module.exports = {
+name: "invite",
+code: `
+$title[1;**Yay**]
+$color[1;$getVar[color]]
+$description[1;**Invite me to your server [Here](https://discord.com/api/oauth2/authorize?client_id=871680066634022923&permissions=8&scope=bot%20applications.commands)**]`
+}
