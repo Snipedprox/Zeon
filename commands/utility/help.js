5fe0191b48e247421c96627aa3b5fac6a99d3a2a
Lexxy <85262576+Snipedprox@users.noreply.github.com>
2022-01-02 18:58:51 -0300

    Create help.js

---
+++ commands/utility/help.js
@@ -0,0 +1,21 @@
+module.exports = {
+name: "help",
+code: `
+$title[1;<:sdtaa:924435792514928651> **My command list**]
+$thumbnail[1;$useravatart[$clientid]]
+$color[1;$getVar[color]]
+$description[1;
+<:redcoins:924345284610363402> **Economy**
+\`apply\` **,** \`balance\` **,** \`beg\` **,** \`buy\` **,** \`deposit\` **,** \`hack\` **,** \`inventory\` **,** \`joblist\` **,** \`shop\` **,** \`withdraw\` **,** \`work\` **,** \`pc\` **,** \`pc stats\` **,** \`pc fix\` **,** \`use\` **,** \`daily\` **,** \`weekly\` **,** \`hourly\` **,** \`profile\` **,** \`pay\` **,** \`badges\` \`rod stats\` **,** \`rod list\` **,** \`rod repair\` **,** \`fishes\` **,** \`fish\` **,** \`sell\` **,** \`fupgrade\` **,** \`travel\` **,** \`adventure\` **,** \`open\` **,** \`promocode\` **,** \`rob\` **,** \`ores\` **,** \`mine\` **,** \`supgrade\` **,** \`plant\` **,** \`pray\` **,** \`premium\` **,** \`cthrow\` **,** \`flex\` **,** \`farm\`
+
+<:moderation:924453423653478440> **Moderation**
+\`alt\` **,** \`clear\` **,** \`nuke\` **,** \`prefix\`
+
+<:utils:924453722543751228> **Utility**
+\`avatar\` **,** \`invite\` **,** \`help\` **,** \`uptime\` **,** \`ping\` **,** \`rank\`  **,** \`celebrate\`
+
+<:premiumcrown:924691807969894410> **Premium**
+\`panik\` **,** \`achievement\` **,** \`gun\` **,** \`glitch\` **,** \`chars\` **,** \`embed\` **,** \`file\`
+
+<:owner:925919650980712478> **Developer**
+\`eval\` **,** \`uc\` **,** \`setavatar\` **,** \`setname\` **,** \`set\` **,** \`guilds\` **,** \`symbol\`]`}
